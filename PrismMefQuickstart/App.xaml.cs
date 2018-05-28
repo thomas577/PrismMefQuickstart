1a6c68012376044c09c15a7867a13090f30a466b
thomas577 <thomas.dier@outlook.com>
2018-05-28 22:55:21 +0100

    Creating the template solution

---
+++ PrismMefQuickstart/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PrismMefQuickstart
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			base.OnStartup(e);
+
+			// The boostrapper will create the Shell instance, so the App.xaml does not have a StartupUri.
+			Bootstrapper bootstrapper = new Bootstrapper();
+			bootstrapper.Run();
+		}
+	}
+}
